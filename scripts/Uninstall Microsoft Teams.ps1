7d9f5488dc4ea72a89df4421af06e6d24270f725
xcgx <36234354+xcgx@users.noreply.github.com>
2021-11-25 04:20:27 +0800

    Create Uninstall Microsoft Teams.ps1

---
+++ scripts/Uninstall Microsoft Teams.ps1
@@ -0,0 +1,42 @@
+#Function to generate a timestamp that is added to the log file
+function Get-TimeStamp {
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)   
+    }
+    
+    #Function to generate a log file
+    if ((Test-Path -Path "$ENV:SystemDrive\Scripts" -PathType Container) -ne $true ) {mkdir "$ENV:SystemDrive\Scripts" | Out-Null}
+    $LogFile = "$ENV:SystemDrive\Scripts\Logs.log"
+    Function LogWrite
+    {
+      Param ([string]$logstring)
+      Add-content $Logfile -value "$(Get-Timestamp) $logstring"
+    }
+
+LogWrite "** STARTING Uninstall MS Teams Script **"
+
+    # Removal Machine-Wide Installer - This needs to be done before removing the EXE!
+    Try {
+        Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -eq "Teams Machine-wide Installer"} | Remove-WmiObject
+    } Catch {
+        LogWrite "Teams Machine-Wide Installer not found."
+        LogWrite "** ENDING Uninstall MS Teams Script **"
+        exit
+    }
+
+    #Variables
+    $TeamsUsers = Get-ChildItem -Path "$($ENV:SystemDrive)\Users"
+
+     $TeamsUsers | ForEach-Object {
+        Try { 
+            if (Test-Path "$($ENV:SystemDrive)\Users\$($_.Name)\AppData\Local\Microsoft\Teams") {
+                LogWrite "Teams found for user $($_.Name), uninstalling..."
+                Start-Process -FilePath "$($ENV:SystemDrive)\Users\$($_.Name)\AppData\Local\Microsoft\Teams\Update.exe" -ArgumentList "-uninstall -s" -EA Stop
+            }
+        } Catch { 
+            LogWrite "Teams app Uninstall for user $($_.Name) Failed! Error Message:"
+            LogWrite $_.Exception.Message
+            Out-Null
+        }
+    }
+
+    LogWrite "** ENDING Uninstall MS Teams Script **"
